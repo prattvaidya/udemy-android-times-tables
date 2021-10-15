51d298ad52e573b691709e6e38a872d412a97514
Pratt Vaidya <pratt.vaidya@ttt.studio>
2021-10-14 17:41:37 -0700

    Add ListView with SeekBar to generate tables from 1 to 10

--- app/src/main/java/ttt/prattvaidya/timestables/MainActivity.java
+++ app/src/main/java/ttt/prattvaidya/timestables/MainActivity.java
@@ -1,14 +1,65 @@
 package ttt.prattvaidya.timestables;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SeekBar;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    ListView listView;
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SeekBar seekBar = findViewById(R.id.seekBar);
+        listView = findViewById(R.id.listView);
+
+        seekBar.setMin(1);
+        seekBar.setMax(20);
+        seekBar.setProgress(1);
+
+        generateTable(1);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                Log.i("progress", Integer.toString(progress));
+
+                generateTable(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+
+    public void generateTable(int tableNumber) {
+        ArrayList<String> tableContent = new ArrayList<String>();
+
+        for (int i = 1; i <= 10; i++) {
+            tableContent.add(Integer.toString(tableNumber * i));
+        }
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, tableContent);
+
+        listView.setAdapter(arrayAdapter);
     }
 }
